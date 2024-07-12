88f4751389a5cff516e97eb5f997ccab8d1fddc3
ATeerioja <teerioja.opiskelu@gmail.com>
2024-07-12 13:40:38 +0300

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(repeatedString, timesRepeat) {
+  if(timesRepeat < 0) {
+    return "ERROR";
+  }
+  
+  
+  let i = 0;
+  let compoundString  = "";
+  while(i < timesRepeat) {
+    compoundString += repeatedString;
+    i++
+  }
+  return compoundString;
 };
 
 // Do not edit below this line
