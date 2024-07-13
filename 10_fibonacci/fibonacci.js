bf2fca671cc9b996ceb4824904befb48f93c8ac4
ATeerioja <teerioja.opiskelu@gmail.com>
2024-07-13 17:01:31 +0300

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(stopHere) {
+
+  let start = 1;
+  let second = 1;
+
+  if(stopHere < 0) return "OOPS";
+  if(stopHere == 0) return 0;
+  if(stopHere <= 2) return 1;
+
+  for(let i = 2; i < stopHere; i++) {
+    sum = start + second
+    start = second
+    second = sum
+  }
+
+  return sum;
+
 
 };
 
