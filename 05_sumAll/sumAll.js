14f33e2c3888a358dd1ff93a502ec088c09250c6
ATeerioja <teerioja.opiskelu@gmail.com>
2024-07-13 15:39:13 +0300

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+  let sum = 0;
+
+  if(start > end) {
+    let newEnd = start;
+    start = end;
+    end = newEnd;
+  }
+
+  if((typeof start != "number" || typeof end != "number")) {
+    return "ERROR";
+  } else if(start < 0 || end < 0) {
+    return "ERROR"
+  }
+
+  for(let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 
 };
 
