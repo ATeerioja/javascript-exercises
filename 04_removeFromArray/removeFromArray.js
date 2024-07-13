e13630b7efde5d8e67b05575982cf5307710e5c3
ATeerioja <teerioja.opiskelu@gmail.com>
2024-07-13 15:26:10 +0300

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeThis) {
+  
+  let shortArray = array;
+
+  for (let remove of removeThis) {
+    while(shortArray.indexOf(remove) > -1) {
+      shortArray.splice((array.indexOf(remove)), 1);
+    }
+
+  }
+  
+  return shortArray
+
 };
 
 // Do not edit below this line
